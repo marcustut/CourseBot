23a6d3bb27b84e4fb004178c1f14377406cb43b2
Marcus Lee <marcustutorial@hotmail.com>
2020-08-14 01:12:44 +0800

    Changed Procfile settings

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ const app = require('express')();
 app.get('/', (req, res) => res.send('Server is up.'));
 
 module.exports = () => {
-  app.listen(process.env.PORT || 3000);
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Express server listening on port ${this.address().port} in ${app.settings.env} mode`);
+  });
 }
